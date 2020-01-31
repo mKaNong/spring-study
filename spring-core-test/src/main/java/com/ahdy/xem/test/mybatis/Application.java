fc1bab75028d658cfc3e19a7812e76345d73dbd8
kanong <ahdyxem@163.com>
2020-01-31 10:06:36 +0800

    date 2020 01 31

--- spring-core-test/src/main/java/com/ahdy/xem/test/mybatis/Application.java
+++ spring-core-test/src/main/java/com/ahdy/xem/test/mybatis/Application.java
@@ -3,6 +3,7 @@ package com.ahdy.xem.test.mybatis;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = {"com.ahdy.xem.test.mybatis"})
 @MapperScan(basePackages = "com.ahdy.xem.test.*.dao")
